cd367c0c81dd41e389e5f31417a67f226b83a4ff
Justin1257-67 <hora9158@bears.unco.edu>
2025-11-04 04:06:57 +0000

    Submision 0.1

---
+++ Activity 5.py
@@ -0,0 +1,53 @@
+words_File = open('CROSSWD.txt , r')
+Print(words_file)
+print(type(words_file.readline))
+for l in dir()(words_file.)):
+    #if k'__' not in k 
+   print([x for x in dir(words_file) if 'read' in x])
+
+print(words_file.readline())
+print(words_file.readline())
+print(words_file.readline())
+print(words_file.readline())
+print(words_file.readline())
+for i in range(15):
+    words.append(words_file.readline().strip())
+print(words)
+
+       # Words_file = open ('crosswrd txt,'r)
+       # for i in words_File
+
+def more_than_20(file):
+    if len (x) > 20
+    ret_list.appemd(x)
+return ret_list 
+
+# section 2 
+
+def has_no_e(word):
+     if "e" in word return False 
+ has_no_e('Alex') # should Return true
+    
+     return 'e' not in word  
+         print(has_no_e)('Alex')
+      
+
+
+      uses_only ('alex Alexis') #
+        Should return true
+       uses_only ('alex Alexas') #
+        Should return false
+
+        If word == alex
+         than if word contains A,L,E,X print true 
+
+         Def uses_only()
+         uses_only('Alex' , 'alexis')
+         #should only return true 
+         def uses_only (word,letters)
+            for letter in word
+            if letter not in word 
+            retern false 
+  return true 
+
+
